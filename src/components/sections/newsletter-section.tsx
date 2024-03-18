eaab4aa71546456b41ce310bb0e969c93826650d
dillytitan <80501424+dillytitan@users.noreply.github.com>
2024-03-17 22:51:00 -0700

    Update newsletter-section.tsx

--- src/components/sections/newsletter-section.tsx
+++ src/components/sections/newsletter-section.tsx
@@ -6,9 +6,9 @@ export function NewsletterSection(): JSX.Element {
     <section
       id="newsletter-section"
       aria-label="newsletter section"
-      className="w-full bg-background md:py-10"
+      className="w-full bg-background md:py-10 min-h-screen"
     >
-      <div className="mx-auto flex flex-col items-center justify-center md:gap-8 md:px-4">
+      <div className="mx-auto flex flex-col md:justify-center md:items-center md:gap-8 md:px-4 min-h-screen pt-8">
         <img src="/orange-cube-main.png" alt="Your GIF" className="mb-2 md:flex md:max-w-3xl" />
 
         <div className="flex flex-col items-center gap-6 text-center">
