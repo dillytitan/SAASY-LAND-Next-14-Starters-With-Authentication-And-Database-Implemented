b778c5285807175bf035a4d279cd1a9ca313db9b
dillytitan <80501424+dillytitan@users.noreply.github.com>
2024-03-20 01:39:57 -0700

    ASIIC Update

--- src/components/sections/newsletter-section.tsx
+++ src/components/sections/newsletter-section.tsx
@@ -6,15 +6,15 @@ export function NewsletterSection(): JSX.Element {
     <section
       id="newsletter-section"
       aria-label="newsletter section"
-      className="w-full bg-background md:py-10 min-h-screen"
+      className="min-h-screen w-full bg-background md:py-10"
     >
-      <div className="mx-auto flex flex-col md:justify-center md:items-center md:gap-8 md:px-4 min-h-screen pt-8">
-        <img src="/orange-cube-main.png" alt="Your GIF" className="mb-2 md:flex md:max-w-3xl gap-6" />
+      <div className="mx-auto flex min-h-screen flex-col pt-8 md:items-center md:justify-center md:gap-8 md:px-4">
+        <img src="/orange-cube-main.png" alt="Your GIF" className="mb-2 gap-6 md:flex md:max-w-4xl" />
 
         <div className="flex flex-col items-center gap-6 text-center">
           <h2 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
             <Balancer>
-              <span className="px-4 pt-6 font-michroma md:flex text-3xl md:text-7xl gap-6">
+              <span className="gap-6 px-4 pt-6 font-michroma text-3xl md:flex md:text-7xl">
                 ORANGE CUBE
               </span>
             </Balancer>
@@ -26,13 +26,13 @@ export function NewsletterSection(): JSX.Element {
           </h3>
         </div>
 
-        <div className=" w-full max-w-lg items-center justify-center md:flex md:max-w-xl">
+        <div className=" w-full max-w-lg items-center justify-center md:flex md:max-w-4xl">
         
-        
+        <Terminal />
           {/* <NewsletterSignUpForm /> */}
         </div>
       
-  <Terminal />
+  
 
       </div>
     </section>
