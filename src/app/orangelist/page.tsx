abaab776ce986d6e45eef66f6ff1a95c53ea0a1f
dillytitan <80501424+dillytitan@users.noreply.github.com>
2024-12-04 19:40:28 -0800

    Orangelist Update

---
+++ src/app/orangelist/page.tsx
@@ -0,0 +1,11 @@
+import OrangeListComponent from "@/components/forms/orangeListComponent";
+
+export default function OrangeList() {
+  return (
+    <div className="flex min-h-screen items-center justify-center overflow-x-hidden bg-black p-4 font-mono text-orange-500 sm:p-8">
+      <div className="mx-auto max-w-sm text-center">
+        <OrangeListComponent />
+      </div>
+    </div>
+  );
+}
